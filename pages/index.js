8213d004a0d8d7a29e0b6b7e04eec3298ad0473f
kinjal-savitara <kinjal@savitarainfotel.com>
2025-10-08 10:37:33 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,72 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import  { useState, useEffect }  from "react";
 
 export default function Home() {
+  const [formData, setFormData] = useState({ name: "", email: "", phone: "" });
+  const [submitted, setSubmitted] = useState(false);
+  const [iframeUrl, setIframeUrl] = useState("");
+
+   
+
+
+  useEffect(() => {
+    document.documentElement.style.height = "100%";
+    document.body.style.height = "100%";
+    document.body.style.margin = "0";
+
+    // Get query param "tour"
+    const params = new URLSearchParams(window.location.search);
+    const tour = params.get("tour");
+
+    // Map query param to iframe links
+    let url = "https://tours.savitarrealty.in/virtualtour/e6b7ba87"; // default
+    if (tour === "virtual-tour") {
+      url = "https://tours.savitarrealty.in/virtualtour/c346bf5c";
+    } else if (tour === "virtual-tour") {
+      url = "https://tours.savitarrealty.in/virtualtour/c346bf5c";
+    }
+
+    setIframeUrl(url);
+  }, []);
+
+  
+  
+    const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  const subject = encodeURIComponent("New Lead from Virtual Tour");
+  const body = encodeURIComponent(
+    `Name: ${formData.name}\nEmail: ${formData.email}\nPhone: ${formData.phone}`
+  );
+
+  window.location.href = `mailto:kinjal@yopmail.com?subject=${subject}&body=${body}`;
+};
+  
   return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
+     <div style={{ position: "fixed", top: 0, left: 0, width: "100%", height: "100%", zIndex: 9999 }}>
+    
+    {iframeUrl != ''?
+      <iframe 
+        src={iframeUrl}
+        frameBorder="0"
+        scrolling="no"
+        width="100%"
+        height="100%"
+        allow="vr;xr-spatial-tracking;gyroscope;accelerometer;magnetometer;"
+        title="Kalyan Heritage"
+        style={{ display: "block", border: "none" }}
+      ></iframe>:null}
+ 
+         
     </div>
+
+
+   
   )
 }
